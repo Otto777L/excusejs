77b2520174417efa5a83e48427d0bdbe9cee80fc
Otto777L <otto.lh12@gmail.com>
2023-01-12 00:58:00 +0000

    Excuse with style

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function generateExcuse() {
+    let excuse = [];
+    let who = ["El alien", "Mi conejo", "Su amigo", "Mi jirafa"];
+    let action = ["se comió", "se orinó", "aplastó", "desintegró"];
+    let what = ["mi tarea", "las llaves", "mi bolso"];
+    let when = [
+      "antes de la clase",
+      "mientras comía",
+      "al llegar a casa",
+      "mientras estaba rezando",
+      "cuando salía de casa"
+    ];
+    excuse =
+      who[Math.floor(Math.random() * who.length)] +
+      ` ` +
+      action[Math.floor(Math.random() * action.length)] +
+      ` ` +
+      what[Math.floor(Math.random() * what.length)] +
+      ` ` +
+      when[Math.floor(Math.random() * when.length)];
+    return excuse;
+  }
+  let html = (document.getElementById("excuse").innerHTML = generateExcuse());
 };
